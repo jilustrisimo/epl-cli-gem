891ef133914bf6c1ac59e7a1410eab84a715a72f
github username <github email address>
2017-01-25 13:04:36 -0500

    continued stubbing out code, testing CLI with hardcoded objects, once completed, will begin building scraper.

--- lib/epl_cli_gem/cli.rb
+++ lib/epl_cli_gem/cli.rb
@@ -19,22 +19,41 @@ class EplCliGem::CLI
 
   def ask_for_team
     puts "Which team would you like more info on?"
-    puts "You can select by rank or name."
+    puts "You can select by rank or team name."
     input = gets.strip
-    if input.to_i == 1
-      puts "Chelsea ..."
-    elsif input.to_i == 2
-      puts "Arsenal..."
-    else
-      puts "Invalid Entry"
-      self.ask_for_team
+
+    if input.to_i == 0
+      team = @teams.find{|team| team.name.downcase == input.downcase}
+      if team != nil then print_team(team) else ask_for_team end
+
+    else input.to_i.between?(1, 20)
+      team = @teams.find{|team| team.rank == input}
+      print_team(team)
+    # elsif input.to_i == 2
+    #   puts "Arsenal..."
+    # else
+    #   puts "Invalid Entry"
+    #   ask_for_team
     end
     puts "Would you like to learn more about another team?"
     more_info = gets.strip
     list_teams if more_info.downcase == 'y' || more_info.downcase == 'yes'
   end
 
+  def print_team(team)
+    puts "\n-------------------#{team.name}----------------------\n"
+    puts "Position:           #{team.rank}"
+    puts "Points:             #{team.points}"
+    puts "Stadium:            #{team.stadium}"
+    puts "Website:            #{team.website}\n\n"
+  end
+
   def bye
     puts "For Club and Country"
   end
 end
+
+
+# if input == 0
+#   #iterate over @@all and find matching team.name
+#   EplCliGem::Team.all.find{|team| team.name == input}
