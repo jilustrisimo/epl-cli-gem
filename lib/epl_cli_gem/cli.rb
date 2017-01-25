b89e921b6cd4a2ea0b8fe91a77123f82062a18bb
github username <github email address>
2017-01-25 13:52:20 -0500

    finished designing CLI, beginning scraper

--- lib/epl_cli_gem/cli.rb
+++ lib/epl_cli_gem/cli.rb
@@ -2,8 +2,6 @@ class EplCliGem::CLI
 
   def call
     list_teams
-    # ask_for_team
-    bye
   end
 
   def list_teams
@@ -13,7 +11,6 @@ class EplCliGem::CLI
     @teams.each.with_index(1) do |team, i|
       puts "   #{i}  #{team.name}  #{team.games_played}  -  #{team.goal_diff}  -  #{team.points}"
     end
-
     ask_for_team
   end
 
@@ -24,24 +21,23 @@ class EplCliGem::CLI
 
     if input.to_i == 0
       team = @teams.find{|team| team.name.downcase == input.downcase}
-      if team != nil then print_team(team) else ask_for_team end
+      if team != nil then print_team(team) else spell_check end
 
-    else input.to_i.between?(1, 20)
+    elsif input.to_i.between?(1, 20)
       team = @teams.find{|team| team.rank == input}
       print_team(team)
-    # elsif input.to_i == 2
-    #   puts "Arsenal..."
-    # else
-    #   puts "Invalid Entry"
-    #   ask_for_team
+
+    else
+      spell_check
     end
-    puts "Would you like to learn more about another team?"
-    more_info = gets.strip
-    list_teams if more_info.downcase == 'y' || more_info.downcase == 'yes'
+
+    puts "Would you like to learn more about another team?\nYes\nNo"
+    more = gets.strip
+    if more.downcase == 'y' || more.downcase == 'yes' then list_teams else bye end
   end
 
   def print_team(team)
-    puts "\n-------------------#{team.name}----------------------\n"
+    puts "\n-------------------#{team.name}----------------------"
     puts "Position:           #{team.rank}"
     puts "Points:             #{team.points}"
     puts "Stadium:            #{team.stadium}"
@@ -50,10 +46,15 @@ class EplCliGem::CLI
 
   def bye
     puts "For Club and Country"
+    sleep 2
+    abort
   end
+
+private
+
+  def spell_check
+    puts "\nInvalid Entry, please check spelling or \nselect a # between 1 and 20.\n\n"
+    ask_for_team
+  end
+
 end
-
-
-# if input == 0
-#   #iterate over @@all and find matching team.name
-#   EplCliGem::Team.all.find{|team| team.name == input}
